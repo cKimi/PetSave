67a1161ee22e3badbbcc6cfea0da04fd5fa8fd8c
Carlos Kimura <ckimura@gmail.com>
2023-12-27 16:24:44 -0300

    API constants created

---
+++ PetSave/PetSave/Core/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  PetSave
+//
+//  Created by Carlos Kimura on 27/12/23.
+//
+
+enum APIConstants {
+    static let host = "api.petfinder.com"
+    static let grantType = "client_credentials"
+    static let clientId = "YourKeyHere"
+    static let clientSecret = "YourSecretHere"
+}
