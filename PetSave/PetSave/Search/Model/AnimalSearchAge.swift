a9f85ba451af27ac2026ec304dd7e086d843cda3
Carlos Kimura <ckimura@gmail.com>
2023-12-27 18:30:28 -0300

    Search models created

---
+++ PetSave/PetSave/Search/Model/AnimalSearchAge.swift
@@ -0,0 +1,14 @@
+//
+//  AnimalSearchAge.swift
+//  PetSave
+//
+//  Created by Carlos Kimura on 27/12/23.
+//
+
+enum AnimalSearchAge: String, CaseIterable {
+    case none
+    case baby
+    case young
+    case adult
+    case senior
+}
---
+++ PetSave/PetSave/Search/Model/AnimalSearchType.swift
@@ -0,0 +1,46 @@
+//
+//  AnimalSearchType.swift
+//  PetSave
+//
+//  Created by Carlos Kimura on 27/12/23.
+//
+
+import SwiftUI
+
+enum AnimalSearchType: String, CaseIterable {
+    case none
+    case cat
+    case dog
+    case rabbit
+    case smallAndFurry = "small & furry"
+    case horse
+    case bird
+    case scalesFinsAndOther = "scales, fins & other"
+    case barnyard
+}
+
+// MARK: - Return appropriate animal images
+extension AnimalSearchType {
+    var suggestionImage: Image {
+        switch self {
+        case .smallAndFurry:
+            return Image("smallAndFurry")
+        case .scalesFinsAndOther:
+            return Image("scalesFinsAndOther")
+        default:
+            return Image(rawValue)
+        }
+    }
+}
+
+// MARK: - Return suggestions
+extension AnimalSearchType {
+    static var suggestions: [AnimalSearchType] {
+        var suggestions = AnimalSearchType.allCases
+        
+        // Removing 'none' from suggestions
+        suggestions.removeFirst()
+        
+        return suggestions
+    }
+}
