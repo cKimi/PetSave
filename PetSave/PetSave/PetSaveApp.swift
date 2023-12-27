0cd72bf861f9dfb80147dbae062cd314342c7b4d
Carlos Kimura <ckimura@gmail.com>
2023-12-27 16:22:07 -0300

    Initial project files

---
+++ PetSave/PetSave/PetSaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetSaveApp.swift
+//  PetSave
+//
+//  Created by Carlos Kimura on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PetSaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
